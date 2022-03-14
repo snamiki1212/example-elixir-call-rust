1486f1d0638826814467e654ab6200fc4d4d1b11
snamiki1212 <snamiki1212@gmail.com>
2022-03-14 00:56:23 -0700

    feat: add fib by elixir

---
+++ lib/fib_by_elixir.ex
@@ -0,0 +1,11 @@
+defmodule FibByElixir do
+  @doc """
+  fib by Elixir without memoization and not Tail Call Optimization.
+  """
+  @spec fib(num :: integer()) :: integer()
+  def fib(num) when num <= 1 do
+    num
+  end
+
+  def fib(num), do: fib(num - 2) + fib(num - 1)
+end
